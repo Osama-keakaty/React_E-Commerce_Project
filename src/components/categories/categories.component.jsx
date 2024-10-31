300bdfa543a92d18000da07d9d9f335fbccc1a47
Osama-keakaty <osama.k3ekaty2001@gmail.com>
2024-10-31 22:43:11 +0300

    add categories section with its category items

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,19 @@
+import CategoryItem from "../category/category-item.component";
+import './categories.styles.scss';
+
+const Categories = ({categories}) => {
+
+    return (
+        <div className="categories-container">
+
+            {categories.map((category) => (
+                <CategoryItem
+                    category={category}
+                    key={category.id}
+                />))}
+
+        </div>
+    );
+}
+
+export default Categories;
